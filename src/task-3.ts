002dfc94c1efea4d140bea1466118e1cfda1181e
Vadaska <goodstoun@gmail.com>
2026-04-30 19:53:56 +0300

    fix

--- src/task-3.ts
+++ src/task-3.ts
@@ -1,8 +1,13 @@
+interface Product {
+  id: number;
+  title: string;
+}
+
 const usernames: string[] = ["alice", "bob", "charlie"];
 
 const ratings: number[] = [4.5, 3.8, 5];
 
-const products: { id: number; title: string }[] = [
+const products: Product[] = [
   { id: 1, title: "Phone" },
   { id: 2, title: "Laptop" },
 ];
